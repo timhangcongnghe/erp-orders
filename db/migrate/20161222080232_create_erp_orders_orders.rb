e020e469e1fc71cac2830c16d0c0fdc5cab79a11
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-17 17:48:12 -0800

    add status, archived columns for orders table

--- db/migrate/20161222080232_create_erp_orders_orders.rb
+++ db/migrate/20161222080232_create_erp_orders_orders.rb
@@ -3,7 +3,9 @@ class CreateErpOrdersOrders < ActiveRecord::Migration[5.0]
     create_table :erp_orders_orders do |t|
       t.string :code
       t.datetime :order_date
+      t.string :status
       t.string :cache_payment_status
+      t.boolean :archived, default: false
       t.references :creator, index: true, references: :erp_users
       t.references :customer, index: true, references: :erp_contacts_contacts
       t.references :supplier, index: true, references: :erp_contacts_contacts
