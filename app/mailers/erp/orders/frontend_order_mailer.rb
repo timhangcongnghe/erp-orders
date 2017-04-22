44135a1db5828a6030d748cf2bedd9ceb8a89c1a
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-04-21 20:19:35 -0700

    change subject for email

--- app/mailers/erp/orders/frontend_order_mailer.rb
+++ app/mailers/erp/orders/frontend_order_mailer.rb
@@ -4,7 +4,7 @@ module Erp::Orders
       # @todo: How to send an order to another email
       @admin = Erp::Contacts::Contact.first
       @order = order
-      send_email(@admin.email, "[TimHangCongNghe.vn] Thông báo: Có một đơn hàng vừa được đặt trên hệ thống (Mã ĐH: #{@order.code})")
+      send_email(@admin.email, "[TimHangCongNghe.vn] Thông báo: Đơn hàng #{@order.code} vừa được đặt trên hệ thống")
     end
     
     def sending_customer_email_order_confirmation(order)
