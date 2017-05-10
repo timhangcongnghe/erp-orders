0abf59b50dfc3481c85903b542a65883886fad2a
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-05-09 18:18:58 -0700

    recipient emails order update

--- app/mailers/erp/orders/frontend_order_mailer.rb
+++ app/mailers/erp/orders/frontend_order_mailer.rb
@@ -2,14 +2,17 @@ module Erp::Orders
   class FrontendOrderMailer < Erp::ApplicationMailer
     def sending_admin_email_order_confirmation(order)
       # @todo: How to send an order to another email
-      @admin = Erp::Contacts::Contact.first
+      # @admin = Erp::Contacts::Contact.first
+      # @todo static emails
+      @recipients = ['Hùng Nguyễn <hungnt@hoangkhang.com.vn>', 'Luân Phạm <luanpm@hoangkhang.com.vn>', 'Sơn Nguyễn <sonnn@hoangkhang.com.vn>']
+      
       @order = order
-      send_email(@admin.email, "[TimHangCongNghe.vn] Thông báo: Đơn hàng #{@order.code} vừa được đặt trên hệ thống")
+      send_email(@recipients.join("; "), "##{@order.code} - Thông tin đơn hàng vừa được đặt trên hệ thống")
     end
     
     def sending_customer_email_order_confirmation(order)
       @order = order
-      send_email(@order.customer.email, "[TimHangCongNghe.vn] Xác nhận đặt hàng thành công")
+      send_email(@order.customer.email, "##{@order.code} - Xác nhận đặt hàng thành công")
     end
   end
 end
