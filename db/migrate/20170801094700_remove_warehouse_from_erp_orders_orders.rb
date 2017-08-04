49456ad80cab99db5e2749be270215c1c2f25637
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-08-03 23:55:53 -0700

    don't remove warehouse references from orders

--- db/migrate/20170801094700_remove_warehouse_from_erp_orders_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveWarehouseFromErpOrdersOrders < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference :erp_orders_orders, :warehouse, index: true, references: :erp_warehouses_warehouses
-  end
-end
