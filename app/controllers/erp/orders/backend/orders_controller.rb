44a4cc4370f035767fd8d33133c7f33158c1efb5
Luan Pham <luanpm88@gmail.com>
2017-08-16 23:24:00 -0700

    update orders routes

--- app/controllers/erp/orders/backend/orders_controller.rb
+++ app/controllers/erp/orders/backend/orders_controller.rb
@@ -4,28 +4,28 @@ module Erp
       class OrdersController < Erp::Backend::BackendController
         before_action :set_order, only: [:show, :show_list, :edit, :update, :destroy, :set_confirmed, :set_deleted]
         before_action :set_orders, only: [:delete_all, :set_confirmed_all, :set_deleted_all]
-    
+
         # GET /orders
         def index
         end
-        
+
         # POST /orders/list
         def list
           @orders = Order.search(params).paginate(:page => params[:page], :per_page => 10)
-          
+
           render layout: nil
         end
-    
+
         # GET /orders/1
         def show
           @type = @order.sales? ? Erp::Orders::Order::TYPE_SALES_ORDER : Erp::Orders::Order::TYPE_PURCHASE_ORDER
         end
-        
+
         # POST /orders/list
         def show_list
-          
+
         end
-    
+
         # GET /orders/new
         def new
           @order = Order.new
@@ -38,23 +38,41 @@ module Erp
           else
             @order.customer_id = @owner.id
           end
-          
+
+          # Import details list from stocking importing page
+          if params[:side_quantity].present?
+            ids = Erp::Products::Product.pluck(:id).sample(rand(90..250))
+            @products = Erp::Products::Product.where(id: ids).order(:code)
+
+            @products.each do |product|
+              area_type = params[:area].present? ? params[:area] : ['area_side','area_central'].sample
+              total_quantity = area_type == 'area_central' ? params[:central_quantity].to_f : params[:side_quantity].to_f
+
+              @order.order_details.build(
+                product_id: product.id,
+                # product_name: product.code + ' ' + product.get_diameter + ' ' + product.category_name
+                price: product.price,
+                quantity: total_quantity
+              )
+            end
+          end
+
           if request.xhr?
             render '_form', layout: nil, locals: {order: @order}
           end
         end
-    
+
         # GET /orders/1/edit
         def edit
           @type = @order.sales? ? Erp::Orders::Order::TYPE_SALES_ORDER : Erp::Orders::Order::TYPE_PURCHASE_ORDER
         end
-    
+
         # POST /orders
         def create
           @order = Order.new(order_params)
           @order.creator = current_user
           @order.status = Erp::Orders::Order::STATUS_DRAFT
-    
+
           if @order.save
             if request.xhr?
               render json: {
@@ -66,14 +84,18 @@ module Erp
               redirect_to erp_orders.edit_backend_order_path(@order), notice: t('.success')
             end
           else
+            puts "=================================================================="
+          puts @order.errors.to_json
             if request.xhr?
               render '_form', layout: nil, locals: {order: @order}
             else
               render :new
             end
           end
+
+
         end
-    
+
         # PATCH/PUT /orders/1
         def update
           if @order.update(order_params)
@@ -82,7 +104,7 @@ module Erp
                 status: 'success',
                 text: @order.id,
                 value: @order.id
-              }              
+              }
             else
               redirect_to erp_orders.edit_backend_order_path(@order), notice: t('.success')
             end
@@ -90,11 +112,11 @@ module Erp
             render :edit
           end
         end
-    
+
         # DELETE /orders/1
         def destroy
           @order.destroy
-          
+
           respond_to do |format|
             format.html { redirect_to erp_orders.backend_orders_path, notice: t('.success') }
             format.json {
@@ -105,11 +127,11 @@ module Erp
             }
           end
         end
-        
+
         # DELETE /orders/delete_all?ids=1,2,3
-        def delete_all         
+        def delete_all
           @orders.destroy_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -119,7 +141,7 @@ module Erp
             }
           end
         end
-        
+
         # dataselect /orders
         def dataselect
           respond_to do |format|
@@ -128,11 +150,11 @@ module Erp
             }
           end
         end
-        
+
         # Confirmed /orders/set_confirmed?id=1
         def set_confirmed
           @order.set_confirmed
-          
+
           respond_to do |format|
           format.json {
             render json: {
@@ -142,11 +164,11 @@ module Erp
           }
           end
         end
-        
+
         # Deleted /orders/set_deleted?id=1
         def set_deleted
           @order.set_deleted
-          
+
           respond_to do |format|
           format.json {
             render json: {
@@ -156,11 +178,11 @@ module Erp
           }
           end
         end
-        
+
         # Confirmed /orders/set_confirmed_all?ids=1,2,3
         def set_confirmed_all
           @orders.set_confirmed_all
-          
+
           respond_to do |format|
           format.json {
             render json: {
@@ -170,11 +192,11 @@ module Erp
           }
           end
         end
-        
+
         # Deleted /orders/set_deleted_all?ids=1,2,3
         def set_deleted_all
           @orders.set_deleted_all
-          
+
           respond_to do |format|
           format.json {
             render json: {
@@ -184,17 +206,17 @@ module Erp
           }
           end
         end
-    
+
         private
           # Use callbacks to share common setup or constraints between actions.
           def set_order
             @order = Order.find(params[:id])
           end
-          
+
           def set_orders
             @orders = Order.where(id: params[:ids])
           end
-    
+
           # Only allow a trusted parameter "white list" through.
           def order_params
             params.fetch(:order, {}).permit(:code, :order_date, :customer_id, :supplier_id, :employee_id, :warehouse_id, :note,
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Erp::Orders::Engine.routes.draw do
 					put 'set_deleted'
 					put 'set_confirmed_all'
 					put 'set_deleted_all'
+					get 'new/:type', :to => "orders#new", :as => 'new_type'
         end
       end
       resources :order_details do
