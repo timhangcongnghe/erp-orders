9a95fff2a8c5d741c1631ea59dd1bacdc6c9c8ab
Luan Pham <luanpm88@gmail.com>
2017-12-05 11:13:58 +0700

    update paitent state to orders:

--- app/controllers/erp/orders/backend/orders_controller.rb
+++ app/controllers/erp/orders/backend/orders_controller.rb
@@ -259,7 +259,7 @@ module Erp
 
           # Only allow a trusted parameter "white list" through.
           def order_params
-            params.fetch(:order, {}).permit(:is_new_patient, :patient_id, :doctor_id, :hospital_id, :code, :order_date, :customer_id, :supplier_id, :employee_id, :warehouse_id, :note, :tax_id, :payment_for,
+            params.fetch(:order, {}).permit(:patient_state_id, :is_new_patient, :patient_id, :doctor_id, :hospital_id, :code, :order_date, :customer_id, :supplier_id, :employee_id, :warehouse_id, :note, :tax_id, :payment_for,
                                             :order_details_attributes => [ :id, :product_id, :order_id, :quantity, :price, :discount, :shipping_fee,
                                                                           :commission, :customer_commission, :description, :serials, :eye_position, :_destroy ])
           end
