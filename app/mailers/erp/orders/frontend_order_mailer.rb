73de26fd2b3fb06224f00fbbf236c0a4b20f3117
Nguyen Ngoc Son <sonnn0811@gmail.com>
2020-04-06 10:41:28 +0000

    fix mailer

--- app/mailers/erp/orders/frontend_order_mailer.rb
+++ app/mailers/erp/orders/frontend_order_mailer.rb
@@ -10,7 +10,7 @@ module Erp::Orders
       # @todo: How to send an order to another email
       # @admin = Erp::Contacts::Contact.first
       # @todo static emails
-      @recipients = ['Hùng Nguyễn <hungnt@hoangkhang.com.vn>', 'Luân Phạm <luanpm@hoangkhang.com.vn>', 'Sơn Nguyễn <sonnn@hoangkhang.com.vn>']
+      @recipients = ['Kinh Doanh <kinhdoanh@hoangkhang.com.vn>', 'Trương Thị Thanh Huyền <huyenttt@hoangkhang.com.vn>', 'Đỗ Thị Đa Nguyên <nguyendtd@hoangkhang.com.vn>', 'Sơn Nguyễn <sonnn@hoangkhang.com.vn>']
 
       @order = order
       send_email(@recipients.join("; "), "##{@order.code} - Thông tin đơn hàng vừa được đặt trên hệ thống")
